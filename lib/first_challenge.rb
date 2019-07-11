7eba87d0e65dcaf7d79ff6435768d9b26adff4db
Alice Brunel <alc.brunel@gmail.com>
2019-07-11 19:38:25 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -6,11 +6,11 @@ def first_challenge
         :hero
         :hero_friends =>[]
       },
-      :capulet => {
-        :patriarch
-        :matriarch
-        :heroine
-        :heroine_friends =>[]
+      :capulet => {      
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
       }
    }
 end
