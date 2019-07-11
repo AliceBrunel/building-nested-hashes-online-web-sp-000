a36448b4720fd17e8feb5913cc74de9c2e52903f
Alice Brunel <alc.brunel@gmail.com>
2019-07-11 19:50:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,4 @@
 def second_challenge
   epic-tragedy = {
-   :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-   }
   }
 end
